4d755f9184588d8c8d83a2590f7f9d09babea9ec
DLitaoen <apl8761@students.uc-bcf.edu.ph>
2024-01-29 23:55:18 +0800

    Used Copilot to add experimental code; need further study

---
+++ 2024 01 29 - Practice/scripts.js
@@ -0,0 +1,8 @@
+// Get the submit button element
+const submitButton = document.getElementById('submit-button');
+
+// Add event listener to the submit button
+submitButton.addEventListener('click', function() {
+    // Display a pop-up message
+    alert('Form submitted!');
+});
