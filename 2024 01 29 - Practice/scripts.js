1be27ae05966bbfa3b077a702bd0a0b2e3ff0242
DLitaoen <apl8761@students.uc-bcf.edu.ph>
2024-01-31 19:11:07 +0800

    Modified files to test JS alert for form submission

--- 2024 01 29 - Practice/scripts.js
+++
@@ -1,8 +0,0 @@
-// Get the submit button element
-const submitButton = document.getElementById('submit-button');
-
-// Add event listener to the submit button
-submitButton.addEventListener('click', function() {
-    // Display a pop-up message
-    alert('Form submitted!');
-});
---
+++ 2024 01 29 - Practice/scripts/alertFormSubmitted.js
@@ -0,0 +1,3 @@
+document.getElementById('button-submit').onclick = function() {
+    alert('Form submitted!');
+}
