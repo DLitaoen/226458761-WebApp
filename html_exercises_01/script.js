028f130e41920c2f2d012cc4ab79d871e87699af
DLitaoen <apl8761@students.uc-bcf.edu.ph>
2024-02-02 18:09:46 +0800

    Modified form validation java script

--- html_exercises_01/script.js
+++ html_exercises_01/script.js
@@ -9,6 +9,58 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // Alert for Successful Form Submission
-document.getElementById('button-submit').onclick = function() {
-    alert('Form successfully submitted. Thank you!');
+document.getElementById('button-submit').onclick = function(event) {
+    event.preventDefault();
+
+    if (formValidation()) {
+        alert('Form successfully submitted. Thank you!');
+    }    
 };
+
+// Form Validation
+function formValidation() {
+    var email = document.getElementById('email-input').value;
+    var username = document.getElementById('username-input').value;
+    var message = document.getElementById('password-input').value;
+    var gender = document.querySelectorAll('input[name="gender-input"]:checked');
+    var termsAndConditions = document.getElementById('terms-conditions-input');
+
+    if (!emailValidation(email)) {
+        alert('Please enter a valid email address.');
+        return false;
+    }
+
+    if (!usernameValidation(username)) {
+        alert('Please enter a valid username.');
+        return false;
+    }
+
+    if (!passwordValidation(password)) {
+        alert('Please enter a valid password.');
+        return false;
+    }
+
+    if (gender.length === 0) {
+        alert('Please select a gender.');
+        return false;
+    }
+
+    if (!termsAndConditions.checked) {
+        alert('Please accept the Terms and Conditions.');
+        return false;
+    }
+
+    return true;
+
+    function emailValidation(email) {
+        return email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/);
+    }
+
+    function usernameValidation(username) {
+        return username.match(/^[0-9A-Za-z]{6,15}$/);
+    }
+    
+    function passwordValidation(password) {
+    return password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,20}$/);
+    }
+}
